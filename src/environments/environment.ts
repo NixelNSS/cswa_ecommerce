16b34852c8667f8906acdc708153aec4829a7c29
Nikola Kostic <nixel@pop-os.localdomain>
2021-05-10 21:18:12 +0200

    Add firebase configuration.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBm6CGeN99X7FrHl609X4MxqvKwN3Ej4GM",
+    authDomain: "cswaecommerce.firebaseapp.com",
+    projectId: "cswaecommerce",
+    storageBucket: "cswaecommerce.appspot.com",
+    messagingSenderId: "13456590225",
+    appId: "1:13456590225:web:06a33d06d8a85e94294630",
+    measurementId: "G-ZKH5EGEK35"
+  }
 };
 
 /*
