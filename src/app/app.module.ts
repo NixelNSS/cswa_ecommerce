f3ed9c9b2e8de2c9f2366bcb0491a8af5491171e
Nikola Kostic <nixel@pop-os.localdomain>
2021-05-10 21:16:53 +0200

    Refactor unknown.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AngularFireModule } from "@angular/fire";
 import { AngularFireAuthModule } from "@angular/fire/auth";
 import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { environment } from "src/environments/environment";
 import { AppRoutingModule } from './routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
@@ -21,7 +25,11 @@ import { RegisterComponent } from './auth/register/register.component';
     AngularFireAuthModule,
     AngularFirestoreModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    FormsModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from "@angular/core";
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-    imports: [],
-    exports: []
+    imports: [
+        MatIconModule,
+        MatFormFieldModule,
+        MatButtonModule,
+        MatInputModule,
+    ],
+    exports: [
+        MatIconModule,
+        MatFormFieldModule,
+        MatButtonModule,
+        MatInputModule,
+    ]
 })
 export class MaterialModule {
 
